001f9f76d697b85a4b7ac8d7bc7f7c9515516a93
dongchangYoo <yoodc8167@gmail.com>
2023-01-12 21:31:20 +0900

    BN-847 fast-relayer, slow-relayer, general-relayer

--- rbclib/utils.py
+++ rbclib/utils.py
@@ -4,7 +4,7 @@ import sys
 # TODO dev function
 def log_invalid_flow(logger, event):
     caller_func_name = sys._getframe(1).f_code.co_name
-    logger.warning("Invalid flow: {} called when handling {} by {}-th relayer".format(
+    logger.warning("Invalid flow: {} called when handling {} by {}".format(
         caller_func_name,
         event.summary(),
         event.manager.active_account.address
