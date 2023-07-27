e5c52c602b4f00134b8ae794d0159a3f201d895c
Min-seong Kwon <kwonarseus@gmail.com>
2023-07-27 13:06:57 +0900

    Update setup.py
    Co-authored-by: Alex Won <dnjscksdn98@gmail.com>

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     version=__version__,
     packages=["rbclib"],
     install_requires=[
-        "chainpy @ git+https://github.com/bifrost-platform/bifrost-python-lib.git@0.7.12"
+        "chainpy @ git+https://github.com/bifrost-platform/bifrost-python-lib.git@0.7.13"
     ]
 )
